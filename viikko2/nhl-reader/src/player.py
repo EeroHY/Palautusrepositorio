2f7a629e690d9437357edca8e1054b7a8bb665c2
Eero Lumijärvi <eero.lumijarvi@helsinki.fi>
2024-11-11 23:01:24 +0200

    Most of exercise 11

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.team = dict['team']
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.games = dict['games']
+        self.id = dict['id']
     def __str__(self):
-        return self.name
+        return self.name + " team " + self.team + " goals " + self.goals
